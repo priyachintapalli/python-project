6b454f9f34d3fadecfae43a56c2a1298010b9d35
priyachintapalli <priyachintapalli95@gmail.com>
2026-04-21 21:46:46 +0530

    Create 06_largest_three.py

---
+++ basics/basics/basics/06_largest_three.py
@@ -0,0 +1,9 @@
+a=int(input("enter a no:"))
+b=int(input("enter a no:"))
+c=int(input("enter a no:"))
+if a>=b and a>=c:
+    print("largest no is: a")
+elif b>=a and b>=c:
+    print("largest no is: b")
+else:
+    print("largest no is: c")
